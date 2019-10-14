e0ef6e0136c5806ca5becf4224738832e0ef82da
Riley Moore <rilemoore@gmail.com>
2019-10-14 22:06:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,15 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
 end
 
 def swap_elements(array)
