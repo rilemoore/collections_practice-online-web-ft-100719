53a1992d8bcc1d540df87f01c130d4225ef62994
Riley Moore <rilemoore@gmail.com>
2019-10-14 22:15:18 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,88 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length < b.length
+      -1
+    elsif a.length > b.length
+      1
+    end
+  end
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  return array
+end
+
+def reverse_array(array)
+  reversed_array = []
+  i = array.length - 1
+  while(i > -1)
+    reversed_array << array[i]
+    i -= 1
+  end
+  return reversed_array
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    word[2] = "$"
+    new_array << word
+  end
+  
+  new_array
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  sum = 0
+  array.each do |num|
+    sum += num
+  end
+  sum
+end
+
+def add_s(array)
+  i = 0
+  while(i < array.length)
+    if(i != 1)
+      array[i] << "s"
+    end
+    i += 1
+  end
+  array
+end
+
+
+
+
+
+
+
+
 
