2e1310cc69ee537dca880c89b20e6fc2a760537b
Riley Moore <rilemoore@gmail.com>
2019-10-14 22:04:25 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,14 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort[]
+  array.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
 end
 
 def sort_array_char_count(array)
