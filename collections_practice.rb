b6ba887db97edce0c6797185891318682854d5d3
Riley Moore <rilemoore@gmail.com>
2019-10-14 21:59:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
+def sort_array_asc(array)
+end
 
+def sort_array_desc(array)
+end
+
+def sort_array_char_count(array)
+end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+end
